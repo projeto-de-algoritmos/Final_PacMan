92e6d568b5ad9c5d60dd7a47b541da4689b9903a
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-04-27 19:51:15 -0300

    Key movements added
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- Pacman/events.py
+++ Pacman/events.py
@@ -1,6 +1,6 @@
-from pygame import QUIT, MOUSEBUTTONDOWN, MOUSEBUTTONUP
+from pygame import QUIT, MOUSEBUTTONDOWN, MOUSEBUTTONUP, KEYDOWN, KEYUP, K_LEFT, K_RIGHT, K_UP,K_DOWN
 from pygame import mouse
-from config import screen, board
+from config import screen, board, movement, player
 
 def treats_event(event) -> bool:
     """treats pygame events
@@ -14,4 +14,23 @@ def treats_event(event) -> bool:
         return True
     elif event.type == MOUSEBUTTONDOWN:
         x, y = mouse.get_pos()
+    elif event.type == KEYDOWN:
+        if event.key == K_LEFT:
+            player.control(-movement,0)
+        if event.key == K_RIGHT:
+            player.control(movement,0)
+        if event.key == K_UP:
+            player.control(0,-movement)
+        if event.key == K_DOWN:
+            player.control(0,movement)
+    elif event.type == KEYUP:
+        if event.key == K_LEFT:
+            player.control(movement,0)
+        if event.key == K_RIGHT:
+            player.control(-movement,0)
+        if event.key == K_UP:
+            player.control(0,movement)
+        if event.key == K_DOWN:
+            player.control(0,-movement)
+
     return False
