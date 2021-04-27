fa7b68afa9ef355b83c4c6c9171c36da3e2fac63
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-04-27 19:49:54 -0300

    Main class updated for player and maze additions
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- Pacman/main.py
+++ Pacman/main.py
@@ -3,16 +3,25 @@ import pygame
 # Functions
 from events import treats_event
 # Global constants and variables
-from config import screen, clock, board
+from config import screen, clock, board, sprite_list, player
+# Colors
+from colors import STANDARD_COLOR
+
 
 board.draw_grid(screen)
 board.maze_prim(1, 1, screen)
-board.draw_border(screen)
 
 done = False
 while not done:
     # Frame rate
     clock.tick(60)
+    # Fill the screen
+    screen.fill(STANDARD_COLOR)
+    # Draw the player
+    player.update()
+    sprite_list.draw(screen)
+    # Draw the maze
+    board.draw_maze(screen)
     # Update screen
     pygame.display.flip()
     # Treats player interaction
